3c7880c7210576342d4981fad3582270be378ae4
Antonio Espinosa <antonioea@antiun.com>
2015-05-28 16:20:24 +0200

    [FIX] Bugfixes to avoid lost of picking_type_code when change qty of one move_line

--- stock_picking_taxes/models/stock_picking.py
+++ stock_picking_taxes/models/stock_picking.py
@@ -38,48 +38,46 @@ class StockPicking(models.Model):
     currency_id = fields.Many2one(
         string='Sale currency', related='sale_id.currency_id')
 
-    @api.multi
+    @api.one
     @api.depends('move_lines', 'move_lines.product_qty',
                  'move_lines.product_uos_qty')
     def _amount_all(self):
-        for picking in self:
-            picking.amount_untaxed = 0.0
-            picking.amount_total = 0.0
-            val2 = val1 = val = 0.0
-            for line in picking.move_lines:
-                if line.procurement_id and line.procurement_id.sale_line_id:
-                    sale_line = line.procurement_id.sale_line_id
-                    cur = sale_line.order_id.pricelist_id.currency_id
-                    price = sale_line.price_unit * (
-                        1 - (sale_line.discount or 0.0) / 100.0)
-                    taxes = sale_line.tax_id.compute_all(
-                        price, line.product_qty,
-                        sale_line.order_id.partner_invoice_id.id,
-                        line.product_id, sale_line.order_id.partner_id)
-                    val1 += cur.round(taxes['total'])
-                    val += cur.round(taxes['total_included'])
-                    for tax in taxes['taxes']:
-                        val2 += tax.get('amount', 0.0)
-            picking.amount_untaxed = val1
-            picking.amount_tax = val2
-            picking.amount_total = val
+        self.amount_untaxed = 0.0
+        self.amount_total = 0.0
+        val2 = val1 = val = 0.0
+        for line in self.move_lines:
+            if line.procurement_id and line.procurement_id.sale_line_id:
+                sale_line = line.procurement_id.sale_line_id
+                cur = sale_line.order_id.pricelist_id.currency_id
+                price = sale_line.price_unit * (
+                    1 - (sale_line.discount or 0.0) / 100.0)
+                taxes = sale_line.tax_id.compute_all(
+                    price, line.product_qty,
+                    sale_line.order_id.partner_invoice_id.id,
+                    line.product_id, sale_line.order_id.partner_id)
+                val1 += cur.round(taxes['total'])
+                val += cur.round(taxes['total_included'])
+                for tax in taxes['taxes']:
+                    val2 += tax.get('amount', 0.0)
+        self.amount_untaxed = val1
+        self.amount_tax = val2
+        self.amount_total = val
 
-    @api.multi
-    def compute(self, picking):
-        if not picking.sale_id:
+    def _compute_taxes(self):
+        if not self.sale_id:
             return {}
         tax_grouped = {}
-        order = picking.sale_id
+        order = self.sale_id
         currency = order.currency_id.with_context(
             date=order.date_order or fields.Date.context_today(order))
-        for move in picking.move_lines:
+        for move in self.move_lines:
             sale_line = move.procurement_id.sale_line_id
             taxes = sale_line.tax_id.compute_all(
                 sale_line.price_unit, move.product_qty, move.product_id,
-                picking.partner_id)['taxes']
+                self.partner_id)['taxes']
             for tax in taxes:
                 val = {
-                    'picking': picking.id,
+                    'picking': self.id,
                     'name': tax['name'],
                     'amount': tax['amount'],
                     'base': currency.round(tax['price_unit'] *
@@ -99,17 +97,16 @@ class StockPicking(models.Model):
             t['amount'] = currency.round(t['amount'])
         return tax_grouped
 
-    @api.multi
+    @api.one
     @api.depends('amount_untaxed', 'amount_tax', 'amount_total')
     def _calc_taxes(self):
         taxes = tax_obj = self.env['stock.picking.tax']
-        for picking in self:
-            picking.taxes.unlink()
-            for tax in self.compute(picking).values():
-                taxes += tax_obj.create({
-                    'picking': tax['picking'],
-                    'sequence': tax['sequence'],
-                    'name': tax['name'],
-                    'base': tax['base'],
-                    'amount': tax['amount']})
-            picking.taxes = taxes.ids
+        self.taxes.unlink()
+        for tax in self._compute_taxes().values():
+            taxes += tax_obj.create({
+                'picking': tax['picking'],
+                'sequence': tax['sequence'],
+                'name': tax['name'],
+                'base': tax['base'],
+                'amount': tax['amount']})
+        self.taxes = taxes.ids
